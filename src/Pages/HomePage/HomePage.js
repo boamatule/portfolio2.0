9687e463b06e9dda25b092f8fb4c83cd0099eeea
boa <boa.matule@gmail.com>
2021-06-15 14:53:21 +0200

    Fix the index for the removed files.

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { HomeInfo, Projects, Profile } from '../../components';
-// import Projects from '../../components/Projects/Projects';
+import { HomeInfo, Profile } from '../../components';
 import ContactPage from '../ContactPage/ContactPage';
 import PortfolioListPage from '../PortfolioPage/PortfolioListPage';
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
@@ -8,13 +7,7 @@ import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data';
 const HomePage = () => (
   <>
     <Profile />
-    {/* <HomeInfo {...homeObjOne} /> */}
-    {/* <HomeInfo {...homeObjTwo} />
-    <HomeInfo {...homeObjThree} /> */}
-    {/* <Projects /> */}
-    {/* <HomeInfo {...homeObjFour} /> */}
     <PortfolioListPage />
-    {/* <ContactPage /> */}
   </>
 );
 
