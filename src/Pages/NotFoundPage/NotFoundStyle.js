7363edafe0eb89cfbd0d1cb13981ee7e7ad4f87b
boa <boa.matule@gmail.com>
2021-06-23 13:06:26 +0200

    Fix the 404 page

--- src/Pages/NotFoundPage/NotFoundStyle.js
+++ src/Pages/NotFoundPage/NotFoundStyle.js
@@ -2,37 +2,44 @@ import styled from 'styled-components/macro';
 import { Container } from '../../global-styles';
 
 export const ErrorContainer = styled(Container)`
-  padding-top: 100px;
-  margin-left: 300px;
-  /* @media screen and (max-width: 760px) {
-    display: flex;
-    align-items: center;
+  /* padding: 80px 80px 120px 420px;
+  @media screen and (max-width: 760px) {
+    padding: 0px 40px 40px 40px;
     align-content: center;
   } */
+  /* align-content: center;
+  max-width: 100%; */
+  display: flex;
+  justify-content: center;
   ${Container};
 `;
 
 export const imgWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  align-content: center;
-  /* margin-top: 100px; */
-  /* padding: 0px 0px 0px 0px; */
+  /* margin-bottom: 15px;
+  padding-right: 60px;
+  padding-left: 60px; */
+  /* flex: 1; */
+  /* flex-direction: column; */
+  /* justify-content: space-between; */
+  /* max-width: 50%; */
+  /* flex-basis: 50%; */
 
-  @media screen and (max-width: 760px) {
+  @media screen and (max-width: 780px) {
+    max-width: 100%;
+    /* flex-basis: 100%; */
     display: flex;
-    /* align-items: center; */
-    /* align-content: center; */
     justify-content: center;
   }
 `;
 
 export const Img = styled.img`
-  width: auto;
-  height: auto;
+  max-width: 100%;
+  margin-bottom: 15px;
+  padding-right: 60px;
+  padding-left: 60px;
+  padding-top: 60px;
   @media screen and (max-width: 780px) {
     max-width: 100%;
-    flex-basis: 100%;
     display: flex;
     justify-content: center;
   }
