f78104c3cfcc12dca0a04651855daeca5894d608
boa <boa.matule@gmail.com>
2022-01-04 12:59:17 +0100

    Updates packages

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable no-restricted-globals */
-
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
 // for the list of available Workbox modules, or add any other
