33625df6fcc03593b1528a8fb978a9db3dacfdd2
boa <boa.matule@gmail.com>
2023-10-23 02:35:41 +0200

    Finish the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,35 @@
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+	// const [isMobile, setIsMobile] = useState(false);
+
+	// useEffect(() => {
+	// 	// Add a listener for changes to the screen size
+	// 	const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+	// 	// Set the initial value of the `isMobile` state variable
+	// 	setIsMobile(mediaQuery.matches);
+
+	// 	// Define a callback function to handle changes to the media query
+	// 	const handleMediaQueryChange = (event) => {
+	// 		setIsMobile(event.matches);
+	// 	};
+
+	// 	// Add the callback function as a listener for changes to the media query
+	// 	mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+	// 	// Remove the listener when the component is unmounted
+	// 	return () => {
+	// 		mediaQuery.removeEventListener("change", handleMediaQueryChange);
+	// 	};
+	// }, []);
+
 	return (
 		<section className="relative w-full h-screen mx-auto">
-			<div
-				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
-			  >
+			<div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
 				<div className="flex flex-col justify-center items-center mt-5">
 					<div className="w-5 h-5 rounded-full bg-[#915eff]" />
 					<div className="w-1 sm:h-80 h-40 violent-gradient bg-[#915eff]" />
@@ -24,6 +46,24 @@ const Hero = () => {
 				</div>
 			</div>
 			<ComputersCanvas />
+			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+				<a href="#about">
+					<div
+						className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
+          flex justify-center items-start p-2"
+					>
+						<motion.div
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								reapeatType: "loop",
+							}}
+							className="w-3 h-3 bg-secondary rounded-full mb-1"
+						/>
+					</div>
+				</a>
+			</div>
 		</section>
 	);
 };
