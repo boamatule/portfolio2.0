9d79cf12e86b612f20040ebf2e41681af780f0a9
boa <boa.matule@gmail.com>
2023-10-23 12:28:10 +0200

    Creates the skillsSet component. Creating Balls model using the 3D elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,29 +4,6 @@ import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-	// const [isMobile, setIsMobile] = useState(false);
-
-	// useEffect(() => {
-	// 	// Add a listener for changes to the screen size
-	// 	const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-	// 	// Set the initial value of the `isMobile` state variable
-	// 	setIsMobile(mediaQuery.matches);
-
-	// 	// Define a callback function to handle changes to the media query
-	// 	const handleMediaQueryChange = (event) => {
-	// 		setIsMobile(event.matches);
-	// 	};
-
-	// 	// Add the callback function as a listener for changes to the media query
-	// 	mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-	// 	// Remove the listener when the component is unmounted
-	// 	return () => {
-	// 		mediaQuery.removeEventListener("change", handleMediaQueryChange);
-	// 	};
-	// }, []);
-
 	return (
 		<section className="relative w-full h-screen mx-auto">
 			<div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -47,7 +24,7 @@ const Hero = () => {
 			</div>
 			<ComputersCanvas />
 			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-				<a href="#about">
+				<a href="#about" aria-label="scroll to section">
 					<div
 						className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary 
           flex justify-center items-start p-2"
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { technologies } from "../constants";
+import { SectionWrapper } from "../hoc";
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
-}
+	return (
+		<div className="flex flex-row flex-wrap justify-center gap-10">
+			{technologies.map((technology) => (
+				<div key={technology.name} className="w-28 h-28">
+					<BallCanvas icon={technology.icon} color={technology.color} />
+				</div>
+			))}
+		</div>
+	);
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
