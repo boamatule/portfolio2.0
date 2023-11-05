c96d5e527bc030ab816a1b6ef5e9e86fa0212162
boa <boa.matule@gmail.com>
2023-11-05 20:35:21 +0100

    removes the hero image

---
+++ dist/assets/ErrorPage-d3f45d28.js
@@ -0,0 +1,2 @@
+import{j as t,a as e,L as r}from"./index-b0c9de15.js";const l=()=>t("div",{className:"w-full h-screen flex justify-center items-center flex-col",children:[e("h1",{className:"font-bold text-7xl mb-5 text-center",children:"404"}),e(r,{to:"/",className:"hover:text-red-400 font-poppins text-center",children:"Something went extremely wrong - Click here to reload."})]});export{l as default};
+//# sourceMappingURL=ErrorPage-d3f45d28.js.map
--- dist/assets/Feedbacks-64406df1.js
+++
@@ -1,2 +0,0 @@
-import{a as e}from"./index-541256d7.js";const r=()=>e("div",{children:"Feedbacks"});export{r as default};
-//# sourceMappingURL=Feedbacks-64406df1.js.map
